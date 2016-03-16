8c1b68998d83c3f471e28b463d521cc60ec6962d
subertbalazs <balazs.subert@gmail.com>
2016-03-16 11:46:56 +0100

    added setters and getters to the Person class

--- src/Server/Person.java
+++ src/Server/Person.java
@@ -1,6 +1,40 @@
 package Server;
 
+import java.util.List;
+
 public class Person
 {
+	String name;
+	String email;
+	List<Skill> skillSet;
 
+	public String getName()
+	{
+		return name;
+	}
+
+	public void setName(String name)
+	{
+		this.name = name;
+	}
+
+	public String getEmail()
+	{
+		return email;
+	}
+
+	public void setEmail(String email)
+	{
+		this.email = email;
+	}
+
+	public List<Skill> getSkillSet()
+	{
+		return skillSet;
+	}
+
+	public void setSkillSet(List<Skill> skillSet)
+	{
+		this.skillSet = skillSet;
+	}
 }
