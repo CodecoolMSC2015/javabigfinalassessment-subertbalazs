b9eb605d39fd3e24b724e9171eac0de27fa92753
subertbalazs <balazs.subert@gmail.com>
2016-03-16 11:35:21 +0100

    added Skill class

---
+++ src/Server/Skill.java
@@ -0,0 +1,28 @@
+package Server;
+
+public class Skill
+{
+	String name;
+	String description;
+	double rate;
+
+	public Skill(String name, String description)
+	{
+
+	}
+
+	public String getName()
+	{
+		return name;
+	}
+
+	public String getDescription()
+	{
+		return description;
+	}
+
+	public double getRate()
+	{
+		return rate;
+	}
+}
